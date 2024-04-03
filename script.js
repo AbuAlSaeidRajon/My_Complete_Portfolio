f0e574e81a45ed47d92f306305baa9413d2e506c
Saeid Md <khanrajon5@gmail.com>
2024-04-03 19:36:54 +0300

    mobile menu style added break point added

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const menuIcon = document.getElementById('menu-icon');
+// JavaScript for mobile menu toggle
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.getElementById('menu-icon');
     const navbar = document.querySelector('.navbar');
 
-    menuIcon.addEventListener('click', () => {
+    menuIcon.addEventListener('click', function() {
       navbar.classList.toggle('show');
     });
+  });
